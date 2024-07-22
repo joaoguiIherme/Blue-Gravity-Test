b9a0f6a86cbcdf1c1eb9b65e2082515dd2a04de1
João Guilherme Lima Waquim <jgwaquim@hotmail.com>
2024-07-22 00:03:32 -0300

    Add namespace v1 and enhance error handling

--- app/api/base_api.rb
+++ app/api/base_api.rb
@@ -2,9 +2,20 @@ class BaseApi < Grape::API
   prefix 'api'
   format :json
 
-  mount UsersApi
-  mount ContentsApi
-  mount RatingsApi
+  namespace :v1 do
+    mount UsersApi
+    mount ContentsApi
+    mount RatingsApi
+  end
+
+  before do
+    header 'Access-Control-Allow-Origin', '*'
+    header 'Access-Control-Request-Method', '*'
+  end
+
+  rescue_from :all do |e|
+    error!(message: "Internal server error: #{e.message}", status: 500)
+  end
 
   add_swagger_documentation
 end
