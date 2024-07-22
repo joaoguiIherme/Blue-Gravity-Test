b1621090f3c137ed09f221dd88eb5547c6205952
João Guilherme Lima Waquim <jgwaquim@hotmail.com>
2024-07-22 00:02:07 -0300

    Fix before action bug when main module is called

--- app/api/auth_middleware.rb
+++ app/api/auth_middleware.rb
@@ -1,16 +1,22 @@
 module AuthMiddleware
+  SECRET_KEY = ENV['SECRET_KEY_BASE'] || 'fallback_secret_key'
+
   def self.included(base)
     base.before do
-      token = headers['Authorization']&.split(' ')&.last
-      begin
-        payload = JWT.decode(token, 'secret_key', true, { algorithm: 'HS256' }).first
-        @current_user = User.find(payload['user_id'])
-      rescue JWT::DecodeError, ActiveRecord::RecordNotFound
-        error!('Unauthorized', 401)
+      token = headers['Authorization']&.then { |header| header.split(' ').last }
+      if token
+        begin
+          payload = JWT.decode(token, SECRET_KEY, true, algorithm: 'HS256').first
+          @current_user = User.find_by(id: payload['user_id'])
+        rescue JWT::DecodeError, ActiveRecord::RecordNotFound => e
+          error!('Unauthorized', 401)
+          Rails.logger.error("Error decoding token: #{e}")
+        end
+      else
+        error!('Token missing', 401)
       end
     end
   end
-
   def current_user
     @current_user
   end
